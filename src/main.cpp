8e92fe3265c00a263cc81fc0aa1e000b037b45cc
LuizRedGalaxy <luizgustavoglt@gmail.com>
2024-08-14 18:41:47 -0300

    Desenvolvido selection sort e boubble sort

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,13 @@
 
 int main(int argc, char **argv)
 {
+  /*
   printf("sizeof(ITEM_VENDA) = %ld\n", sizeof(ITEM_VENDA));
   gerar_array_iv("teste.dat", 512000, 42);
+  */
+
+ auto f = fopen("", );
+ 
+
   return 0;
 }
